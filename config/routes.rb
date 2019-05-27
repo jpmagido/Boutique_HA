6e42a221add82bc2595df8efcd9eaa46012b5ffd
Se7endz <oussama.bendifallahd@gmail.com>
2019-05-27 16:43:30 +0200

    sign up et login front ouss

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  root to: "devise/sessions#new"
   devise_for :users
-
-resources :items
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
