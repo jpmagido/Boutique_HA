c1df48f4069a2042c4a476b79b0adbfd585ad76e
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-05-27 13:25:44 +0200

    CREATION Table: Carts + MODIF Model: Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+
+	validates :title, presence: :true
+	validates :description, presence: :true
+	validates :price, presence: :true
+	validates :image_url, presence: :true
+
+	has_many :carts
+	#belongs_to :user
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_092843) do
+ActiveRecord::Schema.define(version: 2019_05_27_110659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "item_id", default: 1
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_carts_on_item_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
