d818fdeff657e55c5ca3a2b28445fe773550ac00
alexombre <moutapamalexandre@gmail.com>
2019-05-28 13:05:06 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,20 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :items
-<<<<<<< HEAD
+
   resources :carts
-=======
 
-
-
-
-resources :items
-resources :charges
-
-
-
-
->>>>>>> a65adbce3229f7328db86d2a4cff101c9ad9f371
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
