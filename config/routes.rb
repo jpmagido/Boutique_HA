4152cb063d284acc2108ea2f8cce39663868f38b
Se7endz <oussama.bendifallahd@gmail.com>
2019-05-29 13:27:35 +0200

    sauv

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
+  get 'static_pages/show'
   root to: "items#index"
   devise_for :users
 
-	resources :carts
-	resources :items
-	resources :charges
+  resources :items
+
+
+
+
+resources :carts
+resources :items
+resources :charges
+
+
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
+
